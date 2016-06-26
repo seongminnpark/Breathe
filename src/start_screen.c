b6ef0f34e705e14db1e323c7712f45a20c9e418b
Seongmin Park <seongmi1@andrew.cmu.edu>
2016-06-27 02:40:00 +0900

    Add button handlers to change text.

--- src/start_screen.c
+++ src/start_screen.c
@@ -1,5 +1,9 @@
 #include "start_screen.h"
 
+// Global values.
+int duration;
+int interval;
+
 // Layout elements.
 Window *window_start;
 TextLayer *text_layer_duration; // Exercize duration.
@@ -15,8 +19,14 @@ void init_text_layers(void);
 void deinit_text_layers(void);
 
 // Button configuration functions.
-static void select_click_handler(ClickRecognizerRef recognizer, void *context);
 static void click_configuration_provider(void *context);
+static void duration_click_handler(ClickRecognizerRef recognizer, void *context);
+static void start_click_handler(ClickRecognizerRef recognizer, void *context);
+static void interval_click_handler(ClickRecognizerRef recognizer, void *context);
+
+// Text layer control functions.
+void update_duration_text();
+void update_interval_text();
 
 
 void init_window_start() {
@@ -37,6 +47,10 @@ void deinit_window_start() {
 } 
 
 void load_window_start(Window *window) {
+    // Set initial variables.
+    duration = 1;
+    interval = 4;
+    
     // Add layout elements.
     init_text_layers();
 }
@@ -47,12 +61,27 @@ void unload_window_start(Window *window) {
 }
 
 static void click_configuration_provider(void *context) {
-    ButtonId select_button_id = BUTTON_ID_SELECT;
-    window_single_click_subscribe(select_button_id, select_click_handler);
+    ButtonId duration_button_id = BUTTON_ID_UP;
+    ButtonId start_button_id = BUTTON_ID_SELECT;
+    ButtonId interval_button_id = BUTTON_ID_DOWN;
+    
+    window_single_click_subscribe(duration_button_id, duration_click_handler);
+    window_single_click_subscribe(start_button_id, start_click_handler);
+    window_single_click_subscribe(interval_button_id, interval_click_handler);
 }
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-    
+static void duration_click_handler(ClickRecognizerRef recognizer, void *context) {
+    duration = (duration == 5) ? 1 : duration + 1;
+    update_duration_text();
+}
+
+static void start_click_handler(ClickRecognizerRef recognizer, void *context) {
+ 
+}
+
+static void interval_click_handler(ClickRecognizerRef recognizer, void *context) {
+    interval = (interval == 12) ? 1 : interval + 1;
+    update_interval_text();
 }
 
 void init_text_layers() {
@@ -60,23 +89,39 @@ void init_text_layers() {
     text_layer_start = text_layer_create(GRect(110,80,180,30));
     text_layer_interval = text_layer_create(GRect(50,130,180,30));
     
-    char *string_duration_val = malloc(BUF_SIZE_TIME_VAL * sizeof(char));
-    char *string_duration = malloc(BUF_SIZE_DURATION * sizeof(char));
-    char *string_interval_val = malloc(BUF_SIZE_TIME_VAL * sizeof(char));
-    char *string_interval = malloc(BUF_SIZE_INTERVAL * sizeof(char));
-    snprintf(string_duration_val, BUF_SIZE_TIME_VAL, "%d min", 1);
-    snprintf(string_duration, BUF_SIZE_DURATION, TEXT_DURATION, string_duration_val);
-    snprintf(string_interval_val, BUF_SIZE_TIME_VAL, "%d hour", 1);
-    snprintf(string_interval, BUF_SIZE_INTERVAL, TEXT_INTERVAL, string_interval_val);
-    text_layer_set_text(text_layer_duration, string_duration);
+    update_duration_text();
     text_layer_set_text(text_layer_start, TEXT_START);
-    text_layer_set_text(text_layer_interval, string_interval);
-    free(string_duration_val);
-    free(string_interval_val);
+    update_interval_text();
     
     layer_add_child(window_get_root_layer(window_start), text_layer_get_layer(text_layer_duration));
     layer_add_child(window_get_root_layer(window_start), text_layer_get_layer(text_layer_start));
     layer_add_child(window_get_root_layer(window_start), text_layer_get_layer(text_layer_interval));
+}
+
+void update_duration_text() {
+    char *min_text = (duration == 1) ? "%d min" : "%d mins";
+    char *string_duration_val = malloc(BUF_SIZE_TIME_VAL * sizeof(char));
+    char *string_duration = malloc(BUF_SIZE_DURATION * sizeof(char));  
+    
+    snprintf(string_duration_val, BUF_SIZE_TIME_VAL, min_text, duration);
+    snprintf(string_duration, BUF_SIZE_DURATION, TEXT_DURATION, string_duration_val);
+    
+    text_layer_set_text(text_layer_duration, string_duration);
+    
+    free(string_duration_val);
+}
+
+void update_interval_text() {
+    char *hour_text = (interval == 1) ? "%d hour" : "%d hours";
+    char *string_interval_val = malloc(BUF_SIZE_TIME_VAL * sizeof(char));
+    char *string_interval = malloc(BUF_SIZE_INTERVAL * sizeof(char));
+    
+    snprintf(string_interval_val, BUF_SIZE_TIME_VAL, hour_text, interval);
+    snprintf(string_interval, BUF_SIZE_INTERVAL, TEXT_INTERVAL, string_interval_val);
+    
+    text_layer_set_text(text_layer_interval, string_interval);
+    
+    free(string_interval_val);
 }
 
 void deinit_text_layers() {
